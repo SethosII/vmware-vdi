22f03211b504821b436e05efb756aded583f727f
Paul Jähne <paul.jaehne@gmx.de>
2015-04-08 19:20:21 +0200

    removed unnecessary output

--- scripts/unusedVMs.ps1
+++ scripts/unusedVMs.ps1
@@ -5,7 +5,6 @@
 . "C:\path\scripts\constants.ps1"
 
 foreach ($days in @(30,60,90)) {
-echo $days
 	$date = (Get-Date).addDays(-$days).toString("yyyy.MM.dd") -replace "\.", "/"
 	& $folder"scripts\lastLogin.ps1" $date > $folder"unused\"$days.txt
 }
