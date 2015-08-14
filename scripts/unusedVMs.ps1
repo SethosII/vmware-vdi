d926422d015c1ec01e6f0fd026e13a4530ea272f
Paul Jähne <paul.jaehne@gmx.de>
2015-08-14 14:44:20 +0200

    improved unusedVMs
    added comments
    store also all unused vms for 120 and 150 days

--- scripts/unusedVMs.ps1
+++ scripts/unusedVMs.ps1
@@ -1,13 +1,16 @@
-# purpose: store all vms without login for 30/60/90 days
+﻿# purpose: store all vms without login for 30/60/90/120/150 days
 
 # load necessary plugin
 . "C:\Program Files\VMware\VMware View\Server\extras\PowerShell\add-snapin.ps1"
 . "C:\path\scripts\constants.ps1"
 
-foreach ($days in @(30,60,90)) {
+foreach ($days in @(30,60,90,120,150)) {
 	$date = (Get-Date).addDays(-$days).toString("yyyy.MM.dd") -replace "\.", "/"
+
+	# invoke lastLogin.ps1 for each time span
 	$list = & $folder"scripts\lastLogin.ps1" $date
+
+	# store the data
 	$list | Out-File -FilePath $folder"unused\"$days.txt -encoding "UTF8"
 	$list | Out-File -FilePath $remoteStore"unused\"$days.txt -encoding "UTF8"
 }
-
