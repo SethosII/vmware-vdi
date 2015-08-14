98750fe5be040ec232562e100a2cb8ccd0091824
Paul Jähne <paul.jaehne@gmx.de>
2015-08-14 14:39:10 +0200

    improved vmIP
    added comments
    store output also in remoteStore
    used array instead of string

--- scripts/vmIP.ps1
+++ scripts/vmIP.ps1
@@ -1,18 +1,23 @@
-﻿# purpose: outputs a .csv file containing all VMs associated with a user and their entitled users and the IP adress
+﻿# purpose: outputs a .csv file containing all VMs associated with a user, their entitled users and the IP address
 
 # load necessary plugin
 . "C:\Program Files\VMware\VMware View\Server\extras\PowerShell\add-snapin.ps1"
 . "C:\path\scripts\constants.ps1"
 
+# list is an array
+$list = @()
+
 foreach ($vm in Get-DesktopVM) {
 	if ($vm.user_displayname -ne "") {
+		# get name user and IP address for all vms with assigned user
 		$vmName = $vm.Name
 		$user = $vm.user_displayname
 		$ip = $vm.IPAddress
 
-		$list += $vmName + "," + $user + "," + $ip + "`r`n"
+		$list += $vmName + "," + $user + "," + $ip
 	}
 }
 
-$list | Out-File -FilePath $folder"scripts\vmIP.csv" -encoding "UTF8"
-
+# store the data
+$list | Out-File -FilePath $folder"vmIP.csv" -encoding "UTF8"
+$list | Out-File -FilePath $remoteStore"vmIP.csv" -encoding "UTF8"
