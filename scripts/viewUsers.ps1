f6deaee1e6d2a83b6ebdfc04cc5d12f9a18e97f4
Paul Jähne <paul.jaehne@gmx.de>
2015-08-14 14:40:59 +0200

    improved viewUsers
    added comments
    used array instead of string

--- scripts/viewUsers.ps1
+++ scripts/viewUsers.ps1
@@ -4,14 +4,19 @@
 . "C:\Program Files\VMware\VMware View\Server\extras\PowerShell\add-snapin.ps1"
 . "C:\path\scripts\constants.ps1"
 
+# list is an array
+$list = @()
+
 foreach ($vm in Get-DesktopVM) {
 	if ($vm.user_displayname -ne "") {
+		# get name and user for all vms with assigned user
 		$vmName = $vm.Name
 		$user = $vm.user_displayname
 
-		$list += $vmName + "," + $user + "`r`n"
+		$list += $vmName + "," + $user
 	}
 }
 
+# store the data
+$list | Out-File -FilePath $folder"viewUsers.csv" -encoding "UTF8"
 $list | Out-File -FilePath $remoteStore"viewUsers.csv" -encoding "UTF8"
-
