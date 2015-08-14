e870f0107183a5e0973b66293f30d664a827bd72
Paul Jähne <paul.jaehne@gmx.de>
2015-08-14 14:45:57 +0200

    improved printMatch
    changed parameter match to be mandatory

--- scripts/printMatch.ps1
+++ scripts/printMatch.ps1
@@ -1,20 +1,14 @@
 # purpose: print all vms containing a certain string
 Param(
-	[string]$match
+	[Parameter(Mandatory=$true)][string]$match
 )
 
 # load necessary plugin
 . "C:\Program Files\VMware\VMware View\Server\extras\PowerShell\add-snapin.ps1"
 . "C:\path\scripts\constants.ps1"
 
-if ($match -eq "") {
-	echo "please provide a string to look for matches"
-	exit
-}
-
 foreach ($vm in Get-DesktopVM) {
 	if ($vm.name -match $match) {
 		echo $vm.name
 	}
 }
-
