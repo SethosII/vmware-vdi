53117bc443094bf5b7bf50e914c7ba30b96a7ba7
SethosII <paul.jaehne@gmx.de>
2015-08-31 10:17:23 +0200

    added cleanEntitlements

---
+++ scripts/cleanEntitlements.ps1
@@ -0,0 +1,30 @@
+﻿# purpose: remove all pool entitlements without an assigned vm
+
+# load necessary plugin
+. "C:\Program Files\VMware\VMware View\Server\extras\PowerShell\add-snapin.ps1"
+. "C:\Users\Public\Documents\scripts\constants.ps1"
+
+foreach ($pool in Get-Pool) {
+	# arrays
+	$remove = @()
+	$user = @()
+
+	# get all users
+	foreach ($vm in Get-DesktopVM -Pool_id $pool.pool_id) {
+		if ($vm.user_displayname -ne "") {
+			$user += $vm.user_displayname
+		}
+	}
+
+	# check if entitlement has a user
+	foreach ($entitlement in Get-PoolEntitlement -Pool_id $pool.pool_id) {
+		if ($entitlement.displayName -notin $user) {
+			$remove += $entitlement.sid
+		}
+	}
+
+	# remove entitlements without a user
+	foreach ($sid in $remove) {
+		Remove-PoolEntitlement -Pool_id $pool.pool_id -Sid $sid
+	}
+}
